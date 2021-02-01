a0eebd752b11ade63a8d701d1ee3a3c2e0228752
Damiano <belluccidamiano@gmail.com>
2021-02-01 22:37:33 +0100

    readed path of data

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,5 +1,9 @@
 package com.example.demo;
 
+import java.io.File;
+import java.net.URL;
+import java.nio.file.Files;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,7 +12,12 @@ public class HelloController {
 
     @RequestMapping("/")
     public String index() {
-        return "Helloooooo!";
+        try {
+            URL url = getClass().getResource("data.txt");
+            File file = new File(url.getPath());
+            return file.toString();
+        } catch (Exception e) {
+            return "failure";
+        }
     }
-
 }
