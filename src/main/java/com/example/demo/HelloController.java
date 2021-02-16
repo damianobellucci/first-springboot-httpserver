05e7a3964cca39abad36f45876c353613db9098c
Damiano <belluccidamiano@gmail.com>
2021-02-16 20:21:41 +0100

    added api for show chapters

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,5 +1,9 @@
 package com.example.demo;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.web.bind.annotation.*;
+
+import java.sql.ResultSet;
 
 @RestController
 public class HelloController {
@@ -13,12 +17,12 @@ public class HelloController {
 
     @CrossOrigin(origins = "*")
     @RequestMapping(
-            value = "/",
+            value = "/addChapter",
             method = RequestMethod.POST,
             consumes = "application/json",
             produces = "application/json"
     )
-    public String index(@RequestBody Form form) {
+    public String addChapter(@RequestBody Form form) {
         try {
             String chapter = form.chapter;
             db.statement.executeUpdate("INSERT INTO chapters(name) VALUES('"+chapter+"');");
@@ -27,6 +31,31 @@ public class HelloController {
         } catch (Exception e) {
             e.printStackTrace();
             return "failure";
+        }
+    }
+
+    @CrossOrigin(origins = "*")
+    @RequestMapping(
+            value = "/showChapters",
+            method = RequestMethod.GET,
+            consumes = "application/json",
+            produces = "application/json"
+    )
+    public String showChapters() {
+        try {
+            ResultSet result =  db.statement.executeQuery("SELECT * FROM chapters;");
+            JSONArray array = new JSONArray();
+            while(result.next()){
+                array.put(result.getString(2));
+            }
+            JSONObject json = new JSONObject();
+            json.put("list",array);
+            return json.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+            JSONObject json = new JSONObject();
+            json.put("error","failure");
+            return json.toString();
         }
     }
 }
